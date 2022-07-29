a452757f9878177bf896603946f315b35a492323
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-07-29 14:41:30 -0400

    completed

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,24 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
-    render json: students
+    
+    
+
+    if params.keys.length > 2
+      name = Student.all.find_all {|s| s.last_name.downcase == params[:name].downcase || s.first_name.downcase == params[:name].downcase}
+      render json: name
+    else
+      render json: students
+    end
+  
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end 
+
+  def query
+    byebug
+  end 
 end
